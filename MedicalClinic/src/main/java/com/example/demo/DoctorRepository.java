66464a3ef9992b9d977d1fe981fd9b0753fadc9b
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2023-05-11 13:58:16 +0100

    Landing Page prototype

--- MedicalClinic/src/main/java/com/example/demo/DoctorRepository.java
+++ MedicalClinic/src/main/java/com/example/demo/DoctorRepository.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-//This will be AUTO IMPLEMENTED by Spring into a Bean called departamentoRepository
 //CRUD refers Create, Read, Update, Delete
 
 public interface DoctorRepository extends CrudRepository<Doctor, Integer> {
--- MedicalClinic/src/main/java/com/example/demo/MainController.java
+++ MedicalClinic/src/main/java/com/example/demo/MainController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,10 +25,11 @@ public class MainController {
 	@Autowired 
 	private TreatmentRepository treatmentRepository;
 
-	@RequestMapping(path="/")
-	@ResponseBody
-	public String show4buttons (Model model) {
-
-		return "index.html";
+	@GetMapping("/")
+	public String showLandingPage(Model model) {
+	    return "index";
 	}
+
+
+
 }
